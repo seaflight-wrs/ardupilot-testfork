9f65105f940993944a642f0903a09b2414d563a1
wsutton <will@seaflight.tech>
2023-06-16 15:58:00 -0700

    Initialize GE Mode in GCS Files

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -39,6 +39,7 @@ MAV_MODE GCS_MAVLINK_Plane::base_mode() const
     case Mode::Number::FLY_BY_WIRE_A:
     case Mode::Number::AUTOTUNE:
     case Mode::Number::FLY_BY_WIRE_B:
+	case Mode::Number::GROUND_EFFECT:
 #if HAL_QUADPLANE_ENABLED
     case Mode::Number::QSTABILIZE:
     case Mode::Number::QHOVER:
